c963b89bbf50862f9efd5e36795e9c8734c48cb9
James Salvatore <jimmy.c.salvatore@gmail.com>
2020-06-08 09:42:45 -0500

    style: black fix;

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 import pytest
 from airflow import DAG
 
@@ -6,21 +6,11 @@ from airflow import DAG
 # For more info, check out: https://docs.pytest.org/en/2.7.3/plugins.html?highlight=re
 
 
-@pytest.helpers.register
-def run_task(task, dag):
-    dag.clear()
-    task.run(
-        start_date=dag.default_args["start_date"],
-        end_date=dag.default_args["start_date"],
-    )
-
-
 @pytest.fixture
 def test_dag():
     """Returns a test dag if the operator requires a dag to run in."""
     return DAG(
         "test_dag",
-        default_args={"owner": "airflow",
-                      "start_date": datetime.datetime.today()},
-        schedule_interval=datetime.timedelta(days=1),
+        default_args={"owner": "airflow", "start_date": datetime.today()},
+        schedule_interval=timedelta(days=1),
     )
--- tests/test_sample_operator.py
+++ tests/test_sample_operator.py
@@ -1,13 +1,25 @@
+from datetime import datetime, timedelta
 import pytest
+from airflow.models import TaskInstance
 from operators.sample_operator import CapitalizeLetters
 
 
-def test_capitalize_letters(test_dag):
-    """Tests the capitalize_letters functions."""
-    capitalize_letters = CapitalizeLetters(
-        task_id="capitalize",
-        letters="hey everyone",
-        dag=test_dag,
-    )
+def test_capitalize_letters(test_dag, caplog):
+    """ Tests the capitalize_letters functions.
 
-    pytest.helpers.run_task(task=capitalize_letters, dag=test_dag)
+        To test operators, we need three pieces (with examples used here):
+        1. The dag (test_dag fixture)
+        2. The task (instance of CapitalizeLetters)
+        3. The task instance (ti)
+
+        We execute the task instance and any returned values
+        get stored in `result` for testing. 
+
+    """
+    task = CapitalizeLetters(
+        task_id="capitalize", letters="hey everyone", dag=test_dag,
+    )
+    ti = TaskInstance(task=task, execution_date=datetime.now())
+    result = task.execute(ti.get_template_context())
+
+    assert result == "HEY EVERYONE"
